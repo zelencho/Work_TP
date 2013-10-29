ac29c7eab1a1b48efde0b8b54b7962f6e08d4f31
zelencho <krasi@timepursuit.com>
2013-10-29 17:29:06 +0000

    first git commit

---
+++ connectToSQL/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace connectToSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection myConn = new SqlConnection("user id=Krasi;" + "password = ;server = localhost;" + "Trusted_Connection = yes;" + "database = DevAlpha;" + "connection timeout=30");
+
+            try
+            {
+                myConn.Open();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error on opening the connection {0}", e.ToString());
+                Console.ReadKey();
+            }
+            try
+            {
+                SqlDataReader myReader = null;
+                SqlCommand myCommand = new SqlCommand("SELECT * FROM Country", myConn);
+                myReader = myCommand.ExecuteReader();
+                Console.WriteLine("Country ID, Country Name, Two Letter Code, Three Letter Code");
+                while(myReader.Read())
+                {
+                    Console.WriteLine("{0} {1} {2} {3}", myReader["CountryID"].ToString(), myReader["CountryName"].ToString(), myReader["TwoLetterCode"].ToString(), myReader["ThreeLetterCode"].ToString());
+                } 
+                Console.ReadKey();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Error reading from database {0}", exc.ToString());
+                Console.ReadKey();
+            }
+            try
+            {
+                myConn.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error on closing the connection {0}", ex.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
