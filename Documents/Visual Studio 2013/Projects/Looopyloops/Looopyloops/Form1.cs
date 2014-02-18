305f16dd574402a7515f3885993ae705ea47649b
zelencho <krasi@timepursuit.com>
2014-02-18 19:07:49 +0000

    Test

---
+++ Documents/Visual Studio 2013/Projects/Looopyloops/Looopyloops/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Looopyloops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] names = {"Krasi", "James", "Bob" };
+            foreach(string s in names)
+            {
+                MessageBox.Show(s);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+         /*  while(i < 10)
+            {
+                textBox1.Text += i.ToString(); // this will print the numbers form 0 to 9
+                i++;
+            }*/
+            do
+            {
+                textBox1.Text += i.ToString();
+                i++;
+            }
+            while (i < 10);
+        }
+    }
+}
