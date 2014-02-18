96cee59207e068c625a71813f4263cfadca6c714
zelencho <krasi@timepursuit.com>
2014-02-18 19:04:14 +0000

    Halifax characters practice project

---
+++ HalifaxChar/HalifaxChar/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace HalifaxChar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxCharOne = new System.Windows.Forms.ComboBox();
+            this.comboBoxCharTwo = new System.Windows.Forms.ComboBox();
+            this.comboBoxCharThree = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.charOne = new System.Windows.Forms.Label();
+            this.charTwo = new System.Windows.Forms.Label();
+            this.charThree = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxCharOne
+            // 
+            this.comboBoxCharOne.FormattingEnabled = true;
+            this.comboBoxCharOne.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBoxCharOne.Location = new System.Drawing.Point(43, 144);
+            this.comboBoxCharOne.Name = "comboBoxCharOne";
+            this.comboBoxCharOne.Size = new System.Drawing.Size(27, 21);
+            this.comboBoxCharOne.TabIndex = 0;
+            // 
+            // comboBoxCharTwo
+            // 
+            this.comboBoxCharTwo.FormattingEnabled = true;
+            this.comboBoxCharTwo.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBoxCharTwo.Location = new System.Drawing.Point(229, 144);
+            this.comboBoxCharTwo.Name = "comboBoxCharTwo";
+            this.comboBoxCharTwo.Size = new System.Drawing.Size(26, 21);
+            this.comboBoxCharTwo.TabIndex = 1;
+            // 
+            // comboBoxCharThree
+            // 
+            this.comboBoxCharThree.FormattingEnabled = true;
+            this.comboBoxCharThree.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBoxCharThree.Location = new System.Drawing.Point(414, 144);
+            this.comboBoxCharThree.Name = "comboBoxCharThree";
+            this.comboBoxCharThree.Size = new System.Drawing.Size(30, 21);
+            this.comboBoxCharThree.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please select the correct character based on the number in the string required";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Character in position:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(390, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Character in position:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(226, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Character in position:";
+            // 
+            // charOne
+            // 
+            this.charOne.AutoSize = true;
+            this.charOne.Location = new System.Drawing.Point(141, 121);
+            this.charOne.Name = "charOne";
+            this.charOne.Size = new System.Drawing.Size(0, 13);
+            this.charOne.TabIndex = 7;
+            // 
+            // charTwo
+            // 
+            this.charTwo.AutoSize = true;
+            this.charTwo.Location = new System.Drawing.Point(328, 121);
+            this.charTwo.Name = "charTwo";
+            this.charTwo.Size = new System.Drawing.Size(0, 13);
+            this.charTwo.TabIndex = 8;
+            // 
+            // charThree
+            // 
+            this.charThree.AutoSize = true;
+            this.charThree.Location = new System.Drawing.Point(513, 121);
+            this.charThree.Name = "charThree";
+            this.charThree.Size = new System.Drawing.Size(0, 13);
+            this.charThree.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(491, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Run Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.charThree);
+            this.Controls.Add(this.charTwo);
+            this.Controls.Add(this.charOne);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxCharThree);
+            this.Controls.Add(this.comboBoxCharTwo);
+            this.Controls.Add(this.comboBoxCharOne);
+            this.Name = "Form1";
+            this.Text = "Halifax Character";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxCharOne;
+        private System.Windows.Forms.ComboBox comboBoxCharTwo;
+        private System.Windows.Forms.ComboBox comboBoxCharThree;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label charOne;
+        private System.Windows.Forms.Label charTwo;
+        private System.Windows.Forms.Label charThree;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HalifaxChar/HalifaxChar/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Collections;
+
+namespace HalifaxChar
+{
+    public partial class Form1 : Form
+    {
+        public string str = "1je4na";
+       
+    //    public  char[] charListArray = new char[2];
+
+       public int getStrLenght()
+        {        
+            int len = str.Length;
+            return len;
+        }
+         public void randomNumbers()
+        {
+            Random rnd = new Random();
+            int ranNum;
+
+            int[] randomCharPositions = new int[3];
+
+            for (int i = 0;  i < randomCharPositions.Length; i++)
+            {
+                ranNum = rnd.Next(1, getStrLenght() + 1); // the 1 should ensure that the Random number is not 0
+                                                         // the + 1 should ensure that the value from the lenght is taken
+                randomCharPositions[i] = ranNum;
+                if (randomCharPositions[i] > 0 && randomCharPositions[i - 1] == ranNum) i--;
+            }
+             
+             // need to ensure that the values in the array are unique
+            var allSame = Array.TrueForAll(randomCharPositions, x => x == randomCharPositions[0]);
+
+            if(allSame)
+            {
+                MessageBox.Show("Values in the array are the same. Need to rerun");
+                randomNumbers();
+            }
+            else
+            {
+                charOne.Text = randomCharPositions[0].ToString();
+                charTwo.Text = randomCharPositions[1].ToString();
+                charThree.Text = randomCharPositions[2].ToString();
+            }
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            randomNumbers();
+        }
+    }
+}
